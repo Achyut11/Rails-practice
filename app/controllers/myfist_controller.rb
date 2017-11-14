fb1790362cb83db0433b7a35a0e938486babacef
AchyutDahal <lookmefast1@gmail.com>
2017-11-14 23:59:14 +0545

    initial commit

---
+++ app/controllers/myfist_controller.rb
@@ -0,0 +1,9 @@
+class MyfistController < ApplicationController
+def hello
+	@hello ="world"
+	
+end
+def add
+	@num= 2+5
+end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+	get "myfist/hello"
+		get "myfist/add"
+
+end
+

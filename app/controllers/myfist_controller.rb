715ba0db1f437aefd608b56d9498852cba11b8ba
neymarsabin <reddevil.sabin@gmail.com>
2017-11-15 00:18:26 +0545

    Modify indentation

--- app/controllers/myfist_controller.rb
+++ app/controllers/myfist_controller.rb
@@ -1,9 +1,9 @@
 class MyfistController < ApplicationController
-def hello
-	@hello ="world"
-	
-end
-def add
-	@num= 2+5
-end
+  # write code with indention
+  def hello
+	  @hello ="world"
+  end
+  def add
+	  @num= 2+5
+  end
 end
